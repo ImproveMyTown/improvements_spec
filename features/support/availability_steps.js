c8b00c6327d3dcbbff1390663ac4e33e548706f0
alayor <alayor3@gmail.com>
2017-02-01 19:32:29 -0600

    Setting port to 5000 only when localhost env

--- features/support/availability_steps.js
+++ features/support/availability_steps.js
@@ -5,8 +5,8 @@ var expect = require('expect.js');
 defineSupportCode(function({Given, When, Then}) {
 
   var request;
-  var port = process.env.PORT || 5000;
   var host = process.env.IMPROVEMENTS_URL || "localhost";
+  var port = host === "localhost" ? 5000 : null;
 
   Given('I want to make requests to site', function() {
 
