3162741d6cef5f96323930da39956c24ffc9c7ca
alayor <alayor3@gmail.com>
2017-02-02 22:10:10 -0600

    Removing redirection

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ var app = express();
 app.set('port', (process.env.PORT || 5001));
 
 app.use(express.static(__dirname + '/public'));
-app.use("/", function(req, res, next) {
-   require('./runner');
-   res.redirect("/results.html");
-})
 
 app.listen(app.get('port'), function(){
      console.log('Express server listening on port', app.get('port'))
